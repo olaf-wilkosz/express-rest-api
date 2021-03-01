ca6aaf4aebf8b4e9bec4038a5fcb422a55411e34
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-01 23:40:11 +0100

    Add middleware incl. CORS

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
