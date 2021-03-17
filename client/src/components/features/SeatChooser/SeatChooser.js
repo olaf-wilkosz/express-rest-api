ce9b7d6797c9deb41c1e8dc53e29221efaf9945a
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-17 20:45:22 +0100

    Add audience occupancy to SeatChooser

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -35,7 +35,7 @@ class SeatChooser extends React.Component {
   render() {
 
     const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { requests, seats, chosenDay } = this.props;
 
     return (
       <div>
@@ -45,6 +45,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i + 1))}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} />}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert>}
+        <medium>Free seats: {50 - seats.filter(item => item.day === chosenDay).length}/50</medium>
       </div>
     )
   };
